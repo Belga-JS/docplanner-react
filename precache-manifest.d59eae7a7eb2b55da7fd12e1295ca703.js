0e014bb6ad4732136801d2bfd4aa48b32e70657e
Belga-JS <Meherzi.belga@yahoo.com>
2019-11-26 10:22:31 +0100

    Updates

---
+++ precache-manifest.d59eae7a7eb2b55da7fd12e1295ca703.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb09b5c53f6db6ec28d12709aff7470c",
+    "url": "/docplanner-react/index.html"
+  },
+  {
+    "revision": "48230ecb930cf3fb89c6",
+    "url": "/docplanner-react/static/css/main.450e6585.chunk.css"
+  },
+  {
+    "revision": "f8a43e35d6f8cf784c5b",
+    "url": "/docplanner-react/static/js/2.61f5f17e.chunk.js"
+  },
+  {
+    "revision": "48230ecb930cf3fb89c6",
+    "url": "/docplanner-react/static/js/main.29c5f326.chunk.js"
+  },
+  {
+    "revision": "473e57f6bd90d4e2e1ca",
+    "url": "/docplanner-react/static/js/runtime-main.a188c2cb.js"
+  }
+]);
